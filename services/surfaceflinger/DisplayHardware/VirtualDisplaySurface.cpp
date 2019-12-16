c60ea11f8200eb890938e1df81067a44efbd82da
Bharath Gopal <gopalb@codeaurora.org>
2019-12-16 14:58:07 +0530

    sf: Enable UBWC on virtual display scratch buffer
     Set GRALLOC_USAGE_HW_FB usage flag on virtual display Scratch buffer
     to enable UBWC.
    
     CRs-Fixed: 2587934
    
    Change-Id: I3cd431869073448f08ff6a626e65c21679ce09be
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/surfaceflinger/DisplayHardware/VirtualDisplaySurface.cpp
+++ services/surfaceflinger/DisplayHardware/VirtualDisplaySurface.cpp
@@ -330,6 +330,7 @@ status_t VirtualDisplaySurface::dequeueBuffer(Source source,
 #ifdef QCOM_UM_FAMILY
     // Exclude video encoder usage flag from scratch buffer usage flags.
     if (source == SOURCE_SCRATCH) {
+        usage |= GRALLOC_USAGE_HW_FB;
         usage &= ~(GRALLOC_USAGE_HW_VIDEO_ENCODER);
         VDS_LOGV("dequeueBuffer(%s): updated scratch buffer usage flags=%#" PRIx64,
                 dbgSourceStr(source), usage);
